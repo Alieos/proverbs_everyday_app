d9eee4bb127bad18882b04da8437d610a9ba8ef1
Davina Leong <leong.shi.yun@gmail.com>
2017-04-17 18:26:53 +0800

    - Implemented global menu from app component. - Removed testing menu from tabs module

--- proverbs-everyday/src/app/app.component.ts
+++ proverbs-everyday/src/app/app.component.ts
@@ -1,21 +1,41 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {
+  Platform,
+  NavController,
+  MenuController
+} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { MenuItemModel } from '../models/menu-item.model';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:string = 'Tabs';
+  @ViewChild('nav') navCtrl: NavController;
+
+  menuItems = [
+    new MenuItemModel('Tabs', 'Today\'s Proverb', 'quote'),
+    new MenuItemModel('SelectTranslation', 'Bible Translations', 'book'),
+    new MenuItemModel('Settings', 'Settings', 'settings')
+  ];
 
   constructor(platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen) {
+    splashScreen: SplashScreen,
+    private menuCtrl: MenuController ) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  gotoPage(pageName:string) {
+    //this.navCtrl.push(pageName);
+    this.navCtrl.setRoot(pageName);
+    this.menuCtrl.close();
   }
 
 } //end MyApp class
--- proverbs-everyday/src/pages/tabs/tabs.ts
+++ proverbs-everyday/src/pages/tabs/tabs.ts
@@ -1,11 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage,
   NavController,
-  NavParams,
-  MenuController
+  NavParams
 } from 'ionic-angular';
-
-import { MenuItemModel } from '../../models/menu-item.model';
 
 @IonicPage()
 @Component({
@@ -13,20 +10,9 @@ import { MenuItemModel } from '../../models/menu-item.model';
   templateUrl: 'tabs.html',
 })
 export class Tabs {
-  menuItems = [
-    new MenuItemModel('Tabs', 'Today\'s Proverb', 'quote'),
-    new MenuItemModel('SelectTranslation', 'Bible Translations', 'book'),
-    new MenuItemModel('Settings', 'Settings', 'settings')
-  ];
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    public menuCtrl: MenuController) {}
-
-  gotoPage(pageName:string) {
-    this.navCtrl.push(pageName);
-    this.menuCtrl.close();
-  }
+    public navParams: NavParams) {}
 
 } //end Tabs class
