2df0af611b1fbcc645d99e998e0764ccf0543547
Davina Leong <leong.shi.yun@gmail.com>
2017-05-30 18:15:07 +0800

    - Load translations into an ion list.

---
+++ proverbs-everyday/src/data/translation.interface.ts
@@ -0,0 +1,9 @@
+export interface Translation {
+
+    id:         number;
+    name:       string;
+    abbr:       string;
+    copyright:  string;
+    chapters:   any; //TODO: implement chapter interface
+
+} //end Translation interface
--- proverbs-everyday/src/pages/select-translation/select-translation.ts
+++ proverbs-everyday/src/pages/select-translation/select-translation.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import translations from '../../data/passages';
+import { Translation } from '../../data/translation.interface';
+
 @IonicPage()
 @Component({
   selector: 'page-select-translation',
   templateUrl: 'select-translation.html',
 })
 export class SelectTranslation {
+  translations: Translation[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams) {
+  }
+
+  ngOnInit() {
+    this.translations = translations;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SelectTranslation');
+    console.log(translations);
+  }
+
+  gotoChapter(translation: Translation) {
+    console.log('Translation: ', translation.id, translation.abbr);
   }
 
 } //end SelectTranslation class
