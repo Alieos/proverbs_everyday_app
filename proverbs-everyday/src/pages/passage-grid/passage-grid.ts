a1b28b524b3a66da3080b9488920348e118bfed3
Davina Leong <leong.shi.yun@gmail.com>
2017-06-18 21:26:22 +0800

    Show verses on Grid page.

--- proverbs-everyday/src/pages/passage-grid/passage-grid.ts
+++ proverbs-everyday/src/pages/passage-grid/passage-grid.ts
@@ -3,6 +3,11 @@ import { IonicPage,
   NavController,
   NavParams } from 'ionic-angular';
 
+import { PassagesService } from '../../providers/passages.service';
+import { Translation } from '../../data/translation.interface';
+import { Chapter } from '../../data/chapter.interface';
+import { Verse } from '../../data/verse.interface';
+
 @IonicPage()
 @Component({
   selector: 'page-passage-grid',
@@ -10,7 +15,16 @@ import { IonicPage,
 })
 export class PassageGrid {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private _selectedTranslation: Translation = null;
+  private _selectedChapter: Chapter = null;
+  private _versePassages: Verse[] = null;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private _passagesService: PassagesService) {
+      this._selectedTranslation = this._passagesService.selectedTranslation;
+      this._selectedChapter = this._passagesService.getSelectedChapter();
+      this._versePassages = this._passagesService.getSelectedVerses();
   }
 
   ionViewDidLoad() {
